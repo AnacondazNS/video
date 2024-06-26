d3e56e1c69f7a2a62f516cb4b8c523d046b47cf4
Your Name <you@example.com>
2024-06-26 11:21:19 +0800

    Превью надо

--- database/migrations/2024_05_28_072505_create_videos_table.php
+++ database/migrations/2024_05_28_072505_create_videos_table.php
@@ -20,6 +20,7 @@ return new class extends Migration
             $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');
             $table->string('path');
             $table->string('description');
+            $table->string('preview');
             $table->integer('count_like')->default(0);
             $table->integer('count_dislike')->default(0);
             $table->string('visibility')->default('unban');
